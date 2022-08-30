0196877d3c4b71050f7f98ecf4622844fab4ac44
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-30 12:16:00 +0600

    all work is done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // add dependencies
 const warningDiv = document.getElementById('warning');
 
+
+
+
 const row = document.getElementById('row')
 const searchInput = document.getElementById('searchInput')
 
@@ -45,11 +48,11 @@ if (phones.length===0) {
 
         div.innerHTML = `
         <div class="card">
-        <img src="${image}" class="card-img-top" alt="...">
+        <img src="${image}" class="card-img-top" alt="phones images">
         <div class="card-body">
           <h5 class="card-title">${phone_name}</h5>
           <p class="card-text text-bold text-muted">${brand}</p>
-         
+         <button data-bs-toggle="modal" data-bs-target="#exampleModal"  onclick="loadPhoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
         </div>
       </div>
         
@@ -70,11 +73,17 @@ const processSearch= (dataLimit)=>{
    loadPhone(searchText,dataLimit)
 }
 
+// btn click event
+
+document.getElementById('searchBtn').addEventListener('click',()=>{processSearch()})
 
 
+searchInput.addEventListener('keypress',(e)=>{
 
-searchInput.addEventListener('input',()=>{
-    processSearch(6)
+    if(e.key === "Enter"){
+        processSearch(6)
+    }
+   
 })
 
 
@@ -89,8 +98,78 @@ const toggleSpinner = (isLoading)=>{
     }
 }
 
+// display Phone details
+
+const displayPhoneDetails = phoneDetails =>{
+
+const { releaseDate,mainFeatures,others,name,image} = phoneDetails
+const {storage,chipSet,displaySize,memory}= mainFeatures
+const {Bluetooth,GPS,USB,NFC,Radio,WLAN}= others
+
+
+
+// create a dynamic modal
+
+const detailsModal = document.getElementById('exampleModal');
+detailsModal.innerHTML = ''
+const modal = document.createElement('div')
+modal.classList.add('modal-fullscreen')
+
+
+modal.innerHTML = `
+<div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${name}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body mx-auto">
+        <div class="col-lg-6 hidden ">
+       <div> <img  src="${image}" class="img-fluid" alt="phones images"></div>
+        <div>
+        <h5>Release Date : ${releaseDate}</h5>
+        <p class="text-muted text-bold">Storage : ${storage}</p>
+        <p class="text-muted text-bold">Memory : ${memory}</p>
+        <p class="text-muted text-bold">Chipset : ${chipSet}</p>
+        <p class="text-muted text-bold">display : ${displaySize}</p>
+        <p class="text-muted text-bold">Bluetooth : ${Bluetooth}</p>
+        <p class="text-muted text-bold">GPS : ${GPS}</p>
+        <p class="text-muted text-bold">Radio : ${Radio}</p>
+        <p class="text-muted text-bold">WLAN : ${WLAN}</p>
+        <p class="text-muted text-bold">NFC : ${NFC}</p>
+        </div>
+        
+        </div>
+       
+
+         
+        </div>
+      
+      </div>
+
+`
+detailsModal.appendChild(modal)
+
+
+
+}
+
+
+
+// load phone details
+
+const loadPhoneDetails = async id=>{
+const url = `https://openapi.programming-hero.com/api/phone/${id}`
+
+const res = await fetch(url)
+const data = await res.json()
+displayPhoneDetails(data.data)
+}
+
+
 // show all btn click load all data
 
 document.getElementById('show-btn').addEventListener('click',()=>{
     processSearch()
 })
+
+loadPhone('samsung')
